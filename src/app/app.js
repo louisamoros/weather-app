d3aa65b24780bec918bbb0772fccd910e7bd4107
superalligator <amoros.louis@gmail.com>
2015-02-05 22:28:52 -0600

    Add ui.router.

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,18 @@
     console.log('Polymer is ready to rock!');
   });
 
+  angular.module('angularFullstackApp', [
+  	'ui.router'
+  	])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+  	$urlRouterProvider
+  	.otherwise('/');
+
+  	$locationProvider.html5Mode(true);
+  	$httpProvider.interceptors.push('authInterceptor');
+  })
+
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
